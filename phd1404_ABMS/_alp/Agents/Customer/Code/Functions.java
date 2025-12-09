b72543fbe33210e89211680806ff1ad34e374697
mohammadreza <42059116+seyedmohammadrezaayazi@users.noreply.github.com>
2025-12-09 19:05:35 +0330

    new model for Agent based simulation in Store

---
+++ phd1404_ABMS/_alp/Agents/Customer/Code/Functions.java
@@ -0,0 +1,9 @@
+boolean gogoli()
+{/*ALCODESTART::1765293080435*/
+
+
+
+
+return variable&& variable1 || variable2;
+/*ALCODEEND*/}
+
---
+++ phd1404_ABMS/_alp/Agents/Main/Code/Events.java
@@ -0,0 +1,5 @@
+void event()
+{/*ALCODESTART::1765292830266*/
+add_customers();
+/*ALCODEEND*/}
+
